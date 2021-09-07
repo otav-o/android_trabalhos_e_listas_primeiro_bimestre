20503e65bebd0a95517eb088034749b6807947d8
otav-o <otaviocacadordioscanio@gmail.com>
2021-09-06 22:00:46 -0300

    Fazer o binding dos elementos da tela

--- listas/lista1/Ex03-CalculoSalario/app/src/main/java/com/vianna/ex03_calculosalario/MainActivity.java
+++ listas/lista1/Ex03-CalculoSalario/app/src/main/java/com/vianna/ex03_calculosalario/MainActivity.java
@@ -3,12 +3,41 @@ package com.vianna.ex03_calculosalario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout tiHorasTrabalhadas;
+    TextInputLayout tiSalarioPorHora;
+    TextInputLayout tiNumeroDependentes;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding();
+
+        btnCalcular.setOnClickListener(calcularSalario());
+    }
+
+    private View.OnClickListener calcularSalario() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        };
+    }
+
+    private void binding() {
+        tiHorasTrabalhadas = findViewById(R.id.tiHorasTrabalhadas);
+        tiNumeroDependentes = findViewById(R.id.tiNumeroDependentes);
+        tiSalarioPorHora = findViewById(R.id.tiSalarioPorHora);
+        btnCalcular = findViewById(R.id.button);
     }
 }
