af169950926bb1f42134607513377512046d6573
otav-o <otaviocacadordioscanio@gmail.com>
2021-09-07 22:37:27 -0300

    [Fim] Calcular multas e exibir em um TextView

--- listas/lista1/Ex05-Pescador/app/src/main/java/com/vianna/ex05_pescador/MainActivity.java
+++ listas/lista1/Ex05-Pescador/app/src/main/java/com/vianna/ex05_pescador/MainActivity.java
@@ -3,17 +3,24 @@ package com.vianna.ex05_pescador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.vianna.ex05_pescador.models.Peixe;
 import com.vianna.ex05_pescador.models.RegulamentoPesca;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvLimitePeso, tvMulta;
+    TextView tvLimitePeso, tvMulta, tvSomaMultas;
     Button btnCadastrar;
     TextInputLayout tiPeso;
+
+    List<Peixe> pescados= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +30,32 @@ public class MainActivity extends AppCompatActivity {
         binding();
 
         atualizarInformacoes();
-        // btnCadastrar.setOnClickListener(cadastrarPeixe());
+        btnCadastrar.setOnClickListener(cadastrarPeixe());
+    }
+
+    private View.OnClickListener cadastrarPeixe() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double peso = Double.parseDouble(tiPeso.getEditText().getText().toString());
+                pescados.add(new Peixe(peso));
+
+                Peixe[] peixesArray = new Peixe[pescados.size()];
+                pescados.toArray(peixesArray);
+                tvSomaMultas.setText(String.format("R$%.2f", RegulamentoPesca.calcularMulta(peixesArray)));
+            }
+        };
     }
 
     private void atualizarInformacoes() {
-        tvLimitePeso.setText(String.format("%.2f", RegulamentoPesca.getPesoMaximoPorPescado()));
-        tvMulta.setText(String.format("%.2f", RegulamentoPesca.getMultaPorQuiloExcedente()));
+        tvLimitePeso.setText(String.format("%.2fkg", RegulamentoPesca.getPesoMaximoPorPescado()));
+        tvMulta.setText(String.format("R$%.2f", RegulamentoPesca.getMultaPorQuiloExcedente()));
     }
 
     private void binding() {
         tvLimitePeso = findViewById(R.id.tvLimitePeso);
         tvMulta = findViewById(R.id.tvValorMulta);
+        tvSomaMultas = findViewById(R.id.tvSomaMultas);
         btnCadastrar = findViewById(R.id.btnCadastrar);
         tiPeso = findViewById(R.id.tiPeso);
     }
--- listas/lista1/Ex05-Pescador/app/src/main/java/com/vianna/ex05_pescador/models/RegulamentoPesca.java
+++ listas/lista1/Ex05-Pescador/app/src/main/java/com/vianna/ex05_pescador/models/RegulamentoPesca.java
@@ -2,7 +2,7 @@ package com.vianna.ex05_pescador.models;
 
 public class RegulamentoPesca {
 
-    public double calcularMulta(IPescado[] pescados) {
+    public static double calcularMulta(IPescado[] pescados) {
         double pesoExcedido = 0;
         for (IPescado p : pescados) {
             if (p.getPeso() > pesoMaximoPorPescado)
