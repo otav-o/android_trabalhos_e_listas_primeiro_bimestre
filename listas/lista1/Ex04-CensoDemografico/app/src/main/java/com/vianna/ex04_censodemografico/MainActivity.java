01584257c99929d51369ca314a8cef8f39d251c5
otav-o <otaviocacadordioscanio@gmail.com>
2021-09-07 21:26:38 -0300

    Reformular a main activity

--- listas/lista1/Ex04-CensoDemografico/app/src/main/java/com/vianna/ex04_censodemografico/MainActivity.java
+++ listas/lista1/Ex04-CensoDemografico/app/src/main/java/com/vianna/ex04_censodemografico/MainActivity.java
@@ -2,13 +2,56 @@ package com.vianna.ex04_censodemografico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import com.vianna.ex04_censodemografico.models.Censo;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton rbMasculino, rbFeminino;
+    Button btnCadastrar, btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding();
+    }
+
+    private void registraEventos() {
+        btnCadastrar.setOnClickListener(cadastrarPessoa());
+        btnCalcular.setOnClickListener(calcularResultado());
+    }
+
+    private View.OnClickListener cadastrarPessoa() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
+
+    private View.OnClickListener calcularResultado() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Censo c1 = new Censo(pessoas);
+
+            }
+        };
+    }
+
+    private void binding() {
+        rbFeminino = findViewById(R.id.rbFeminino);
+        rbMasculino = findViewById(R.id.rbMasculino);
+
+        btnCadastrar = findViewById(R.id.btnCadastrarPessoa);
+        btnCalcular = findViewById(R.id.btnCalcular);
     }
 }
