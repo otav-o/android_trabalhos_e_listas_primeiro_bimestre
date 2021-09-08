205cf978a280cc6ba6a97f665c893bc8834e6d73
otav-o <otaviocacadordioscanio@gmail.com>
2021-09-07 21:54:49 -0300

    [Fim] Fazer o binding, salvar dados em um array e calcular os resultados

--- listas/lista1/Ex04-CensoDemografico/app/src/main/java/com/vianna/ex04_censodemografico/MainActivity.java
+++ listas/lista1/Ex04-CensoDemografico/app/src/main/java/com/vianna/ex04_censodemografico/MainActivity.java
@@ -7,13 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.vianna.ex04_censodemografico.models.Censo;
+import com.vianna.ex04_censodemografico.models.Genero;
+import com.vianna.ex04_censodemografico.models.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RadioButton rbMasculino, rbFeminino;
     Button btnCadastrar, btnCalcular;
+    TextInputLayout tiCorCabelos, tiIdade, tiCorOlhos, tiAltura;
+
+    List<Pessoa> listaPessoas = new ArrayList<Pessoa>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         binding();
+
+        registraEventos();
     }
 
     private void registraEventos() {
@@ -32,7 +44,16 @@ public class MainActivity extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Genero g = rbFeminino.isChecked() ? Genero.feminino : Genero.masculino;
+                String corCabelos = tiCorCabelos.getEditText().getText().toString();
+                String corOlhos = tiCorOlhos.getEditText().getText().toString();
+                int idade = Integer.parseInt(tiIdade.getEditText().getText().toString());
+                double altura = Double.parseDouble(tiAltura.getEditText().getText().toString());
 
+                listaPessoas.add(new Pessoa(g, corOlhos, corCabelos, idade, altura));
+
+                Toast.makeText(getApplicationContext(),
+                        String.format("Salvo"), Toast.LENGTH_LONG).show();
             }
         };
     }
@@ -41,8 +62,13 @@ public class MainActivity extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Censo c1 = new Censo(pessoas);
+                Pessoa[] populacao = new Pessoa[listaPessoas.size()];
+                listaPessoas.toArray(populacao);
+                Censo c1 = new Censo(populacao);
 
+                Toast.makeText(getApplicationContext(),
+                        String.format("Dados desta população: "
+                                + c1.retornarPesquisa()), Toast.LENGTH_LONG).show();
             }
         };
     }
@@ -50,6 +76,11 @@ public class MainActivity extends AppCompatActivity {
     private void binding() {
         rbFeminino = findViewById(R.id.rbFeminino);
         rbMasculino = findViewById(R.id.rbMasculino);
+
+        tiAltura = findViewById(R.id.tiAltura);
+        tiCorCabelos = findViewById(R.id.tiCorCabelos);
+        tiIdade = findViewById(R.id.tiIdade);
+        tiCorOlhos = findViewById(R.id.tiCorOlhos);
 
         btnCadastrar = findViewById(R.id.btnCadastrarPessoa);
         btnCalcular = findViewById(R.id.btnCalcular);
