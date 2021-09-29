23d30bb24079dbc7e832a4bfdea67a100f3db9c4
otav-o <otaviocacadordioscanio@gmail.com>
2021-09-28 22:13:23 -0300

    Criar MainActivity
    Ainda incompleta, aguardando demais telas

--- listas/lista2/Ex02-ArtigoCientifico/app/src/main/java/com/vianna/ex02_artigocientifico/MainActivity.java
+++ listas/lista2/Ex02-ArtigoCientifico/app/src/main/java/com/vianna/ex02_artigocientifico/MainActivity.java
@@ -3,12 +3,61 @@ package com.vianna.ex02_artigocientifico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCadastrarArtigo, btnCadastrarEscritor, btnAvaliarArtigo;
+    TextView lblArtigosMaisAvaliados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding();
+        registraEventos();
+    }
+
+    private void registraEventos() {
+        btnAvaliarArtigo.setOnClickListener(callViewAvaliarArtigo());
+        btnCadastrarEscritor.setOnClickListener(callViewCadastrarEscritor());
+        btnCadastrarArtigo.setOnClickListener(callViewCadastrarArtigo());
+    }
+
+    private View.OnClickListener callViewCadastrarArtigo() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: implementar
+            }
+        };
+    }
+
+    private View.OnClickListener callViewCadastrarEscritor() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: implementar
+            }
+        };
+    }
+
+    private View.OnClickListener callViewAvaliarArtigo() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: implementar
+            }
+        };
+    }
+
+    private void binding() {
+        btnCadastrarArtigo = findViewById(R.id.btnSubmeterArtigoView);
+        btnCadastrarEscritor = findViewById(R.id.btnCadastrarEscritorView);
+        btnAvaliarArtigo = findViewById(R.id.btnAvaliarArtigoView);
+
+        lblArtigosMaisAvaliados = findViewById(R.id.tvArtigosCampeoes);
     }
 }
