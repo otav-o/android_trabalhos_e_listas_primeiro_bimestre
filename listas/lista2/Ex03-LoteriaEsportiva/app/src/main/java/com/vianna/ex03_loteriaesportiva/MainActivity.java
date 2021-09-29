b0c6f3910984241560262f4e4c474232610819e2
otav-o <otaviocacadordioscanio@gmail.com>
2021-09-29 20:37:00 -0300

    Criar MainiActivity

--- listas/lista2/Ex03-LoteriaEsportiva/app/src/main/java/com/vianna/ex03_loteriaesportiva/MainActivity.java
+++ listas/lista2/Ex03-LoteriaEsportiva/app/src/main/java/com/vianna/ex03_loteriaesportiva/MainActivity.java
@@ -2,13 +2,40 @@ package com.vianna.ex03_loteriaesportiva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFazerLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding();
+        registrarEventos();
+    }
+
+    private void registrarEventos() {
+        btnFazerLogin.setOnClickListener(callLoginView());
+    }
+
+    private View.OnClickListener callLoginView() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: chamar a outra tela
+                //Intent itn = new Intent(getApplicationContext(), ???.class);
+                //startActivity(itn);
+            }
+        };
+    }
+
+    private void binding() {
+        btnFazerLogin = findViewById(R.id.btnFazerLoginCallView);
     }
 }
